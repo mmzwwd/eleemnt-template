553fec5b4913dc60ae856e1a89d6930a68aceede
wwd <exercise666@163.com>
2020-09-17 16:31:59 +0800

    初始项目

---
+++ src/store/getters.js
@@ -0,0 +1,9 @@
+const getters = {
+  sidebar: state => state.app.sidebar,
+  language: state => state.app.language,
+  device: state => state.app.device,
+  token: state => state.user.token,
+  avatar: state => state.user.avatar,
+  name: state => state.user.name
+}
+export default getters
